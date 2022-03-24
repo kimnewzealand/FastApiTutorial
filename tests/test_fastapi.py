c94db8a49fcb735d57a4e5ac6e4f3de5f7de4e7a
Kim Fitter <kimfitter@yahoo.com>
2022-03-25 10:55:35 +1300

    Remove sample tests

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -13,16 +13,3 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == []
-
-
-class TestClass:
-
-    def test_one(self):
-        print('test_one')
-        x = "this"
-        assert "h" in x
-
-    def test_two(self):
-        print('test_two')
-        x = "hello"
-        assert x == "hello"
