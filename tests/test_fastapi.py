80604b48094cb1d39172629563ce42c1018de77e
Kim Fitter <kimfitter@yahoo.com>
2022-03-28 10:42:43 +1300

    Add session and client fixtures to db

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -1,18 +1,52 @@
+from sqlalchemy.orm import sessionmaker
 import pytest
 
 from app.main import app
+from app.database import Base, engine, get_db
 from fastapi.testclient import TestClient
 import sys
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-client = TestClient(app)
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture()
+def session():
+
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    db = TestingSessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+
+    # Dependency override
+
+    def override_get_db():
+        try:
+
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+
+    yield TestClient(app)
 
 
 @pytest.mark.parametrize("success_path, success_code", [
     ("/", 200),
     ("/posts", 200)
 ])
-def test_read_main(success_path, success_code):
+def test_read_main(client, success_path, success_code):
     response = client.get(success_path)
     assert response.status_code == success_code
